8af2a252541c0c20ce77707aa3de0a5cb0336977
cristiano112715 <cristiano112715@gmail.com>
2023-05-07 00:12:28 -0300

    aula 04

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -8,11 +8,13 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit{
-  repeatPass: string = 'none';
-  RPWD: any;
+  repeatPass: String = 'none';
 
-  displayMsg: string = '';
+
+
+  displayMsg: String = '';
   isAccountCreated: boolean = false;
+  RPWD: any;
 
   constructor(private authService: AuthService){ }
 
@@ -49,36 +51,35 @@ export class RegisterComponent implements OnInit{
     rpwd: new FormControl(''),
   });
 
-    registerSubmited(){
-      if(this.PWD.value == this.RPWD.value){
-        console.log(this.registerForm.valid);
-        this.repeatPass = 'none';
+  registerSubmited(){
+    if (this.PWD.value == this.RPWD.value){
+      console.log(this.registerForm.valid);
+      this.repeatPass = 'none';
 
-        this.authService
-        .registerUser([
-          this.registerForm.value.firstname,
-          this.registerForm.value.lastname,
-          this.registerForm.value.email,
-          this.registerForm.value.telefone,
-          this.registerForm.value.genero,
-          this.registerForm.value.pwd,
-        ])
-        .subscribe(res => {
-          if (res == 'Sucess'){
-            this.displayMsg = 'Account Created Successfully!';
-            this,this.isAccountCreated = true;
-          } else if (res == 'Already Exist') {
-            this.displayMsg = 'Account Already Exist. try another Email.';
-            this.isAccountCreated = false;
-          }else {
-            this.displayMsg = 'Something went wrong. Try Again.';
-            this.isAccountCreated = false;
-          }
-          });
-
-      }else{
-        this.repeatPass = 'inline'
-      }
+      this.authService
+      .registerUser([
+       // this.registerForm.value.firstname,
+       // this.registerForm.value.lastname,
+        //this.registerForm.value.email,
+       // this.registerForm.value.telefone,
+       // this.registerForm.value.genero,
+       // this.registerForm.value.pwd,
+       ])
+      .subscribe(res => {
+        if (res == 'Sucesso'){
+          this.displayMsg = 'Account Created Successfully!';
+          this,this.isAccountCreated = true;
+        } else if (res == 'Already Exist') {
+          this.displayMsg = 'Account Already Exist. try another Email.';
+          this.isAccountCreated = false;
+        }else {
+          this.displayMsg = 'Something went wrong. Try Again.';
+          this.isAccountCreated = false;
+        }
+        });
+    }else{
+      this.repeatPass = 'inline'
+    }
   }
 
   get FirstName(): FormControl {
@@ -99,5 +100,4 @@ export class RegisterComponent implements OnInit{
   get PWD(): FormControl {
     return this.registerForm.get("pwd") as FormControl;
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,24 +6,36 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   baseServerUrl = 'https://localhost:44342/api/';
 
-  registerUser(user: Array<string>){
-   return this.http.post(
-    this.baseServerUrl + 'User/CreateUser',
-   {
-    FisrtName: user[0],
-    LastName: user[1],
-    Email: user[2],
-    Telefone:user[3],
-    genero: user[4],
-    Pwd: user[5]
-  },
-   {
-    responseType: 'text',
-   }
-   );
- }
+    registerUser(user: Array<String>){
+    return this.http.post(
+      this.baseServerUrl + 'User/CreateUser',
+    {
+      FirstName: user[0],
+      LastName: user[1],
+      Email: user[2],
+      Telefone: user[3],
+      Genero: user[4],
+      Pwd: user[5]
+    },
+      {
+        responseType: 'text',
+      }
+    );
+  }
+
+  loginUser(loginInfo: Array<String>){
+    return this.http.post(this.baseServerUrl + 'LoginUser', {
+      Email: loginInfo[0],
+      Pwd: loginInfo[1]
+    },
+    {
+      responseType: 'text',
+    }
+    );
+  }
+
 }
